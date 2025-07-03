0c209dca73e58514b8a45aaf37c55ab9e0d5df82
hanwenbo <realhanwenbo@gmail.com>
2025-07-03 08:16:10 +0800

    build(docker): 更新 Docker 构建脚本和 Dockerfile
    - 新增 build_and_push.sh脚本，用于构建和推送 Docker 镜像
    - 更新 Dockerfile，添加阿里云镜像源并安装额外的 Python 包- 优化 Dockerfile 中的 pip 安装命令，提高安装速度和可靠性

---
+++ docker/amd64/build_and_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set variables
+APP=dify-sandbox
+VERSION=0.2.12.$(date "+%Y%m%d%H%M")
+NAMESPACE=open
+ACR_REGISTRY=yiya-acr-registry.cn-hangzhou.cr.aliyuncs.com
+
+# Build the Docker image
+# The -f flag specifies the path to the Dockerfile relative to the project root
+docker build -f docker/amd64/dockerfile -t ${ACR_REGISTRY}/${NAMESPACE}/${APP}:${VERSION} .
+
+# Push the Docker image
+#docker push ${ACR_REGISTRY}/${NAMESPACE}/${APP}:${VERSION}
